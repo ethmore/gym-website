8413db0f33eb8eed588b3952627c28f48fbe419a
ethmore <cetinermehmetali@gmail.com>
2024-11-22 11:56:17 +0300

    Add: data and data type for program cards

--- app/Components/Programs/Programs.tsx
+++ app/Components/Programs/Programs.tsx
@@ -1,5 +1,32 @@
 import ProgramsCard from "./ProgramsCard";
 
+export type ProgramCard = {
+    href: string;
+    imgSrc: string;
+    imgAlt: string;
+    title: string;
+};
+
+const data: ProgramCard[] = [
+    {
+        href: "/programlar",
+        imgSrc: "./programs2.webp",
+        imgAlt: "egzersiz halkalarıyla şınav çeken sarışın kadın",
+        title: "Evde Fitness",
+    },
+    {
+        href: "/programlar",
+        imgSrc: "./programs1.webp",
+        imgAlt: "bir kutunun üzerine atlayan kadın",
+        title: "Calisthenics",
+    },
+    {
+        href: "/programlar",
+        imgSrc: "./programs3.webp",
+        imgAlt: "bir kadın dambıllarla bench press yapıyor, bir adam ona yardım ediyor",
+        title: "Bodybuilding",
+    },
+];
 export default function Programs() {
     return (
         <section id="programs" className="w-[min(1100px,90%)] m-auto mt-36">
@@ -7,24 +34,17 @@ export default function Programs() {
                 Ücretsiz Egzersiz Programları
             </h1>
             <div className="flex flex-col sm:flex-row gap-8 justify-center justify-items-center mt-16">
-                <ProgramsCard
-                    href="/programlar"
-                    imgSrc="/programs2.webp"
-                    imgAlt="egzersiz halkalarıyla şınav çeken sarışın kadın"
-                    title="Evde Fitness"
-                />
-                <ProgramsCard
-                    href="/programlar"
-                    imgSrc="/programs1.webp"
-                    imgAlt="bir kutunun üzerine atlayan kadın"
-                    title="Calisthenics"
-                />
-                <ProgramsCard
-                    href="/programlar"
-                    imgSrc="/programs3.webp"
-                    imgAlt="bir kadın dambıllarla bench press yapıyor, bir adam ona yardım ediyor"
-                    title="Bodybuilding"
-                />
+                {data.map((item, index) => {
+                    return (
+                        <ProgramsCard
+                            key={index}
+                            href={item.href}
+                            imgSrc={item.imgSrc}
+                            imgAlt={item.imgAlt}
+                            title={item.title}
+                        />
+                    );
+                })}
             </div>
         </section>
     );
